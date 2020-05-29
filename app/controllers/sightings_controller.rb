2eb5ef5c8e95a013a5787c1998017af2ec497d82
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-29 22:48:54 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,7 +1,7 @@
 class SightingsController < ApplicationController
   def index
     sightings = Sighting.all
-    render json: sightings, include: [:bird, :location]
+    render json: sightings, include: [:bird, :location], except: [:updated_at]
   end
 
   def show
