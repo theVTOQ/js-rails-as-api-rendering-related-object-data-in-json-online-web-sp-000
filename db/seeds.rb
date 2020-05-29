15d7e2b9b5b297c3f29fff9ae53ab10116cc78fd
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-29 22:40:54 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ bird_d = Bird.create(name: "Mourning Dove", species: "Zenaida Macroura")
 location_a = Location.create(latitude: "40.730610", longitude: "-73.935242")
 location_b = Location.create(latitude: "30.26715", longitude: "-97.74306")
 location_c = Location.create(latitude: "45.512794", longitude: "-122.679565")
- 
+
 sighting_a = Sighting.create(bird: bird_a, location: location_b)
 sighting_b = Sighting.create(bird: bird_b, location: location_a)
 sighting_c = Sighting.create(bird: bird_c, location: location_a)
