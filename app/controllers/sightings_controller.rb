da20afe6de4bb504f3401dff68bc18c25b9f1f92
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-29 22:48:00 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -12,7 +12,7 @@ class SightingsController < ApplicationController
     if sighting
       render json: sighting, include: [:bird, :location]
     else
-      render json: {message: ""}
+      render json: {message: "No sighting found with that id."}
     end
   end
 end
